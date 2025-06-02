51602d4e3abfc402bd4d6877a1c4c47112f08b3b
Grenzpreis <47915724+Grenzpreis@users.noreply.github.com>
2025-06-02 20:52:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def index():
                            uploaded_file_info=uploaded_file_info, mode=mode)
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
